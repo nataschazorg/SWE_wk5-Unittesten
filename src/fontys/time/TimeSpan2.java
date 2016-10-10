f49a1cc49006b5a85acd7f286b6565c7111a770f
Johan Vergeer <johanvergeer@gmail.com>
2016-10-10 15:32:15 +0200

    Add TimeSpan2 class

---
+++ src/fontys/time/TimeSpan2.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fontys.time;
+
+/**
+ *
+ * @author johan
+ */
+public class TimeSpan2 implements ITimeSpan{
+
+    /* class invariant: 
+     * A stretch of time with a begin time and end time.
+     * The end time is always later then the begin time; the length of the time span is
+     * always positive
+     * 
+     */
+    private ITime bt, et;
+    
+    /**
+     * 
+     * @param bt must be earlier than et
+     * @param et 
+     */
+    public TimeSpan2(ITime bt, ITime et) {
+        if (bt.compareTo(et) <= 0) {
+            throw new IllegalArgumentException("begin time "
+                    + bt + " must be earlier than end time " + et);
+        }
+
+        this.bt = bt;
+        this.et = et;
+    }
+
+    @Override
+    public ITime getBeginTime() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITime getEndTime() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int length() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setBeginTime(ITime beginTime) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEndTime(ITime endTime) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void move(int minutes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void changeLengthWith(int minutes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isPartOf(ITimeSpan timeSpan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITimeSpan unionWith(ITimeSpan timeSpan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITimeSpan intersectionWith(ITimeSpan timeSpan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
